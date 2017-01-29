bc2856bf686990d8b01f35fdf4866a6d4e94cd5a
W. Wesley Weidenhamer II <www.wesley2@gmail.com>
2017-01-29 12:53:47 -0500

    final commit for release

--- web.py
+++ web.py
@@ -8,23 +8,21 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from eventregistry import *
-import sys
-sys.path.insert(0, "C:/Python27/Scripts")
-from client import DiffbotClient, DiffbotCrawl
-from config import API_TOKEN
+from newspaper import Article
 import requests
-import json
 
 app = Flask(__name__)
-json1 = json.loads(request.POST.get('mydata', '{}'))
-json2 = json.loads(request.POST.get('mydata', '{}'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 '''
 Creates index.html
 '''
-@app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html', one="inline", two="none", data1="", data2="")
+    return render_template('index.html', one="inline", two="none", three="none", data1="", data2="", source1="", source2="")
 
 '''
 Receives News Source and Search data that has been entered.
@@ -32,39 +30,32 @@ Queries API for current news matching the search criteria and displays them for 
 '''
 @app.route('/entry', methods=['POST'])
 def entry():
-    language1 = getLanguage(request.form['News Source 1'])
-    language2 = getLanguage(request.form['News Source 2'])
-    search = request.form['search']
+    source1 = request.form['News Source 1']
+    source2 = request.form['News Source 2']
+    language1 = getLanguage(source1)
+    language2 = getLanguage(source2)
+    searchText = request.form['search']
     
-    #send search info to news api for query
-    #parse Titles out of articles
-    #get list of articles in each language
-    #save article json info in memory
-    search = str(search).split(" ")
+    search = str(searchText).split(" ")
     lang1 = language1
     lang2 = language2
     numberOfStories = 5
     er = EventRegistry()
     q = QueryEvents(lang = [lang1,lang2])
 
-    #q = QueryArticles(lang = lang1)
 
-
-    er.login("atolstoy@umd.edu","448d6cfb11")
-
+    er.login("wesleyw@terpmail.umd.edu","ChocolateChipCookies")
+    
     for s in search:
-        if(len(s)>=3):
-            #js = json.dumps(er.suggestConcepts(s, lang = [lang1,lang2], conceptLang= [lang1,lang2],count = 1)[0])
         
-            #print(json.loads(js)["uri"])    
-            #q.addConcept(json.loads(js)["uri"]) #change count to larger number to increase breadth
+        if(len(s)>=3):
             q.addKeyword(s)
 
 
-
+    
     q.addRequestedResult(RequestEventsInfo(page = 1, count = 1, returnInfo = ReturnInfo(eventInfo = EventInfoFlags(concepts = False,categories = False))))
+    
     res =  er.execQuery(q)
-
     selectedEvent = 0
     event = res["events"]["results"][selectedEvent]["uri"] #0 is the index of the event that gets retrieved
  
@@ -74,12 +65,12 @@ def entry():
 
     q3 = QueryEvent(event)
     q3.addRequestedResult(RequestEventArticles(count = numberOfStories, lang = lang2)) 
-    res2 = er.execQuery(q3) 
+    res2 = er.execQuery(q3)
     
-    global json1 = res
-    global json2 = res2
+    article1 = parseTitles(res, event)
+    article2 = parseTitles(res2, event)
     
-    return render_template('index.html', one="none", two="inline", data1=parseTitles(res, event), data2=parseTitles(res2, event))
+    return render_template('index.html', one="none", two="inline", three="none", data1=article1, data2=article2, source1=source1, source2=source2)
 
 '''
 Receives chosen article titles.
@@ -88,27 +79,20 @@ Uses API to get text only from the article and translate everything to English.
 '''
 @app.route('/selection', methods=['POST'])
 def selection():
-     body1 = request.form['article1']
-     body2 = request.form['article2']
-     #get article link from whats saved in memory
-     #convert article to text
-     #send articles to html
-     url1 = getURL(json1, body1)
-     url2 = getURL(json2, body2)
-     
-     diffbot = DiffbotClient()
-     token = "0e2f4a8a541e26e874c0a76a0ff8cad5"
-     version = 2
-     url = "http://www.foxnews.com/us/2017/01/28/custom-officials-enforcing-trump-immigration-ban-at-us-airports.html"
-     api = "article"
-     response = diffbot.request(url, token, api, version=2)
-
-    text = response['text']
-    r = requests.post("http://text-processing.com/api/sentiment/", data = {'text':text})
-    print(r.text)
-    print(text)
-     
-     return render_template('index.html', one="none", two="inline", article1=1, article2=1)
+    url1 = request.form['article1']
+    url2 = request.form['article2']
+    news1 = Article(url=url1)
+    news1.download()
+    news1.parse()
+    news1Array = [news1.title, news1.top_image, news1.text, url1]
+    
+    news2 = Article(url=url2)
+    news2.download()
+    news2.parse()
+    news2Array = [news2.title, news2.top_image, news2.text, url2]
+    
+    
+    return render_template('index.html', one="none", two="none", three="inline", data1=news1Array, data2=news2Array, source1="", source2="")
 
 '''
 Converts news source chosen via front-end into a language the news API can search for
@@ -134,15 +118,9 @@ def getLanguage(source):
 def parseTitles(s, article_id):
     titles = []
     for i in range(0,len(s[article_id]["articles"]["results"])):
-        titles.append(    s[article_id]["articles"]["results"][i]["body"])
+        titles.append(s[article_id]["articles"]["results"][i]["source"]["title"] + ": " +     s[article_id]["articles"]["results"][i]["title"])
+        titles.append(    s[article_id]["articles"]["results"][i]["url"])
     return titles
-
-def getURL(s, body):
-    for key in s.keys():
-        for i in range(0,len(s[key]["articles"]["results"])):
-            if(body == s[key]["articles"]["results"][i]["body"]):
-                return s[key]["articles"]["results"][i]["url"]
-    return
 
 if __name__ == '__main__':
     app.run()
